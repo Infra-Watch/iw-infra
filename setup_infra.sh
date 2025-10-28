465089507d6584c7612ea3edbd5ad38505417fff
Davi Domingues <davi.domingues@sptech.school>
2025-10-28 19:10:46 -0300

    :wrench: fix: script appweb | escrevendo ponto ausente no script

--- setup_infra.sh
+++ setup_infra.sh
@@ -119,7 +119,7 @@ chmod -R 777 ./infraweb/app-node
 # Dockerfile do projeto Node
 # ----------------------------
 echo "Criando Dockerfile do projeto Node..."
-cat << EOF > /infraweb/app-node/iw-appweb/Dockerfile
+cat << EOF > ./infraweb/app-node/iw-appweb/Dockerfile
 FROM node:latest
 WORKDIR /app
 RUN git clone https://github.com/Infra-Watch/iw-appweb.git
